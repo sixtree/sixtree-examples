3e43e3dc4492579cdfef2943e2d35ebbcd1b31b9
robvalk <robert.valk@sixtree.com.au>
2015-02-26 12:04:48 +1100

    Update UserACLExtractor.java

--- mule-spring-security-blog/UserACLExtractor.java
+++ mule-spring-security-blog/UserACLExtractor.java
@@ -7,33 +7,32 @@ import org.springframework.security.ldap.userdetails.LdapUserDetailsImpl;
 
 public class UserACLExtractor implements Callable {
 
-	private static final Logger log = LoggerFactory
-			.getLogger(UserACLExtractor.class);
+  private static final Logger log = LoggerFactory.getLogger(UserACLExtractor.class);
 
-	@Override
-	public Object onCall(MuleEventContext eventContext) throws Exception {
-		String username = null;
-		String userACL = null;
+  @Override
+  public Object onCall(MuleEventContext eventContext) throws Exception {
+    String username = null;
+    String userACL = null;
 
-		// Get access to the userPrincipal
-		Object userPrincipal = eventContext.getSession().getSecurityContext().getAuthentication().getPrincipal();
+    // Get access to the userPrincipal
+    Object userPrincipal = eventContext.getSession().getSecurityContext().getAuthentication().getPrincipal();
 		
-		if (userPrincipal instanceof LdapUserDetailsImpl) {
-			// Get the username
-			username = ((LdapUserDetailsImpl) userPrincipal).getUsername();
+    if (userPrincipal instanceof LdapUserDetailsImpl) {
+      // Get the username
+      username = ((LdapUserDetailsImpl) userPrincipal).getUsername();
 
-			// Risky business to fetch the password
-			String password = ((LdapUserDetailsImpl) userPrincipal).getPassword();
+      // Risky business to fetch the password
+      String password = ((LdapUserDetailsImpl) userPrincipal).getPassword();
 
-			// Get the ACL or list of groups the User belongs to. This is a comma separated list of .
-			userACL = ((LdapUserDetailsImpl) userPrincipal).getAuthorities().toString();
-		}
+      // Get the ACL or list of groups the User belongs to. This is a comma separated list of .
+      userACL = ((LdapUserDetailsImpl) userPrincipal).getAuthorities().toString();
+    }
 
-		log.debug("USER - " + username);
-		log.debug("GRANTED AUTHORITIES - " + userACL);
-		eventContext.getMessage().setProperty("http.request.inbound.user.name", username, PropertyScope.SESSION);
-		eventContext.getMessage().setProperty("http.request.inbound.user.acl", userACL.split(','), PropertyScope.SESSION);
+    log.debug("USER - " + username);
+    log.debug("GRANTED AUTHORITIES - " + userACL);
+    eventContext.getMessage().setProperty("http.request.inbound.user.name", username, PropertyScope.SESSION);
+    eventContext.getMessage().setProperty("http.request.inbound.user.acl", userACL.split(','), PropertyScope.SESSION);
 
-		return eventContext.getMessage();
-	}
+    return eventContext.getMessage();
+  }
 }
